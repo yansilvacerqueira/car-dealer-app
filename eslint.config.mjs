0542900bc9f74f2f5420dd51203ec763a9aca8dc
Yan Cerqueira <yansilvacerqueira@outlook.com>
2025-01-21 13:34:58 -0300

    feat: adding prettier and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,49 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-plugin-prettier";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
+      prettier: prettierConfig,
+    },
+    rules: {
+      // TypeScript rules
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-explicit-any": ["warn"],
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+      // React hooks rules
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+      // React rules
+      "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+
+      // Prettier rules
+      "prettier/prettier": "error",
+    },
+  },
+  pluginJs.configs.recommended, // Include base JS rules
+  ...tseslint.configs.recommended, // TypeScript recommended rules
+  pluginReact.configs.flat.recommended, // React recommended rules
 ];
-
-export default eslintConfig;
